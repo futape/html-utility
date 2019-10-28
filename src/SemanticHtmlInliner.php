f05d00a5a18f76d9785e3ca6cb4ddc6c75785ef9
Lucas Krause <lucas.krause@eventim.de>
2019-10-28 13:49:21 +0100

    BUGFIX: Fix SemanticHtmlInliner::$tagsToRemove initialization
    Fixes the initialization of the $tagsToRemove property of
    SemanticHtmlInliner and removes debug info.

--- src/SemanticHtmlInliner.php
+++ src/SemanticHtmlInliner.php
@@ -6,24 +6,6 @@ namespace Futape\Utility\Html;
 
 class SemanticHtmlInliner extends HtmlInliner
 {
-    /** @var string[] */
-    protected $tagsToRemove = [
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'h5',
-        'h6',
-        'header',
-        'footer',
-        'hgroup',
-        'aside',
-        'figure',
-        'nav',
-        'menu',
-        'legend'
-    ];
-
     /**
      * @param string $html
      */
@@ -31,12 +13,26 @@ class SemanticHtmlInliner extends HtmlInliner
     {
         parent::__construct($html);
 
-
-        var_dump(parent::getTagsToRemove());
-        var_dump($this->getTagsToRemove());
-
-        $this->setTagsToRemove(array_merge(parent::getTagsToRemove(), $this->getTagsToRemove()));
-        var_dump($this->getTagsToRemove());
-
+        $this->setTagsToRemove(
+            array_merge(
+                $this->getTagsToRemove(),
+                [
+                    'h1',
+                    'h2',
+                    'h3',
+                    'h4',
+                    'h5',
+                    'h6',
+                    'header',
+                    'footer',
+                    'hgroup',
+                    'aside',
+                    'figure',
+                    'nav',
+                    'menu',
+                    'legend'
+                ]
+            )
+        );
     }
 }
